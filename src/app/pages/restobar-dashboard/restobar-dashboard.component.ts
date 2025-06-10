2474754b5ade991830debba5a9f798df2232977a
CodeFairo <paulo.castro.berrocal@gmail.com>
2025-06-10 18:47:35 -0500

    arreglando dashboard parte 1

--- src/app/pages/restobar-dashboard/restobar-dashboard.component.ts
+++ src/app/pages/restobar-dashboard/restobar-dashboard.component.ts
@@ -1,10 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-user-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatFormFieldModule,
+            MatInputModule, MatSelectModule,
+            MatCardModule, MatButtonModule            
+            ],
   templateUrl: './restobar-dashboard.component.html',
   styleUrl: './restobar-dashboard.component.css'
 })
